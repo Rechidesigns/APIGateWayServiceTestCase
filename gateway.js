0b36e3bc065cb929d7cc688860751860733c0896
Alerechi Ordu <alerechiordu@gmail.com>
2024-06-13 00:59:59 +0100

    added routes

--- gateway.js
+++ gateway.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const routes = require('./routes')
 
 app.use(express.json())
+app.use('/', routes)
+
 app.listen(PORT, () => {
     console.log('Gateway has started on port ' + PORT )
 })
